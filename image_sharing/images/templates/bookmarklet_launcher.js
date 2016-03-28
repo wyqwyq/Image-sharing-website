d3014a2ce89af1fcbaf2d9aeb5bbb2235cfc0b75
Yunqing Wen <420974020@qq.com>
2016-03-28 21:06:54 +0800

    update

--- image_sharing/images/templates/bookmarklet_launcher.js
+++ image_sharing/images/templates/bookmarklet_launcher.js
@@ -1,9 +1,8 @@
 (function(){
-    if (window.myBookmarklet !== undefined){
+    if(window.myBookmarklet!==undefined){
         myBookmarklet();
-    }else {
-        alert("Loading!")
-        // 随机数避免从浏览器cache中load
-        document.body.appendChild(document.createElement('script')).src='http://mysite:8000/static/js/bookmarklet.js?r='+Math.floor(Math.random()*9999999999999);
+    }
+    else{
+        document.body.appendChild(document.createElement('script')).src='http://mysite.com:8000/static/js/bookmarklet.js?r='+Math.floor(Math.random()*99999999999999999999);
     }
 })();
